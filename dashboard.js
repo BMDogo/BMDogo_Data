70947473292b778c18995473c3fabfd7eece53eb
BMDogo <bashirmmuktar017@gmail.com>
2025-11-13 14:53:16 +0100

    Fix redirection to index.html from dashboard.js
    Updated redirection paths for login and logout.

--- dashboard.js
+++ dashboard.js
@@ -5,7 +5,7 @@ if(logged === 'true') {
    document.getElementById('welcome').textContent = `Welcome Back ${currentUser}!!!`;
 }else {
 setTimeout(() =>{
-   window.location.href = '../index.html';
+   window.location.href = 'index.html';
    },300);
    };
 let logout = document.getElementById('logout');
@@ -14,6 +14,6 @@ function Out() {
     localStorage.removeItem('currentUser');
     localStorage.removeItem('isLoggedIn');
     setTimeout(() => {
-        window.location.href = '../index.html';
+        window.location.href = 'index.html';
     }, 1000)
 }
